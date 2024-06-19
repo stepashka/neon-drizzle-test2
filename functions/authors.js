56358b84d99b63510ac98d14c026991c77550f1f
Anna Stepanyan <anna.stepanyan@zenith.tech>
2024-06-19 17:45:52 +0200

    moving the app code to functions for cloudflare pages compatibility

--- functions/authors.js
+++ functions/authors.js
@@ -1,5 +1,12 @@
-// GET requests to /filename would return "Hello, world!"
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { authors, books } from './schema';
 
 export const onRequestGet = () => {
-  return new Response("Hello, world!")
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+  const sql = neon(DATABASE_URL);
+  const db = drizzle(sql);
+
+  const output = await db.select().from(authors);
+  return new Response(c.json(output))
 }
